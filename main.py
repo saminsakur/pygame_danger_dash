0be2a181c4d24b58449f4a56d709f5155a686ab1
samin <saminsakur06@gmail.com>
2021-08-25 14:27:21 +0600

    Added a moving snake

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ ground_surface = pygame.image.load("graphics/ground.png")
 
 text_surface = text.render("hello world!", False, "black")
 
+snail_surface = pygame.image.load("graphics/snail/snail1.png")
+snail_x_pos = 600
+snail_speed = 4
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -38,8 +42,13 @@ while running:
 
     window.blit(sky_surface, (0, 0))
     window.blit(ground_surface, (0, 300))
-
     window.blit(text_surface, (300, 50))
+
+    snail_x_pos -= snail_speed
+    if snail_x_pos <= -100:
+        snail_x_pos = WIDTH
+
+    window.blit(snail_surface, (snail_x_pos, 260))
 
     pygame.display.update()
     pygame.display.flip()
